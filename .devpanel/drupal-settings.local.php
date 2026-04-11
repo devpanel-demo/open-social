78b8c1b15f1292de661992840ea8b3d103d047c0
vinugawade <vinulike11@gmail.com>
2026-04-12 04:22:54 +0530

    Update PHP version to 8.4 and enhance database configuration with isolation level

--- .devpanel/drupal-settings.local.php
+++ .devpanel/drupal-settings.local.php
@@ -6,14 +6,15 @@
  * There are some basic configuration created by DevPanel
  */
 $databases['default']['default'] = [
-    'database' => getenv('DB_NAME'),
-    'username' => getenv('DB_USER'),
-    'password' => getenv('DB_PASSWORD'),
-    'host' => getenv('DB_HOST'),
-    'port' => getenv('DB_PORT'),
-    'driver' => getenv('DB_DRIVER'),
-    'prefix' => '',
-    'collation' => 'utf8mb4_general_ci',
+    'database'        => getenv('DB_NAME'),
+    'username'        => getenv('DB_USER'),
+    'password'        => getenv('DB_PASSWORD'),
+    'host'            => getenv('DB_HOST'),
+    'port'            => getenv('DB_PORT'),
+    'driver'          => getenv('DB_DRIVER'),
+    'prefix'          => '',
+    'collation'       => 'utf8mb4_general_ci',
+    'isolation_level' => 'READ COMMITTED',
 ];
 
 $settings['file_private_path'] = dirname($app_root) . '/private';
